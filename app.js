034149dbd6392005a3213e2946ad8b26e3097834
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-09-17 22:27:01 -0500

    imprter inquirer and performed a test

--- app.js
+++ app.js
@@ -1,40 +1,35 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-// const userName = profileDataArgs[0];
-// const gitHubName = profileDataArgs[1];
-
-//Assignment Destructuring: assigns elements of an array to variable names in a single expression.
-const [userName, gitHubName] = profileDataArgs;
-
-
-//const profileDataArgs = process.argv.slice(2);
-
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log('================');
-
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-
-//const generatePage = () => 'Name: Alex, Github: AlexJCturbo';
-//const generatePage = (userName, gitHubName) => `Name: ${userName}, GitHub: ${gitHubName}`;
+inquirer
+  .prompt([
+    //question object
+    {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+    }
+  ])
+  //The answer object is returned as a Promise
+  .then((answers) => console.log(answers))
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
 
 
-//console.log(generatePage('Alex', 'AlexJCturbo'));
 
-fs.writeFile('index.html', generatePage(userName, gitHubName), err => {
-  if (err) throw err;
+  
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-  console.log('Portfolio complete! Check out index.html to see the output');
-});
+// const pageHTML = generatePage(userName, gitHubName);
+
+// fs.writeFile('index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output');
+// });
+
